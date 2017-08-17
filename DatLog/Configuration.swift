1fbd53302c810013c01b2629645cbfbc77c9d448
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-08-17 09:02:00 +0300

    * Oh wow, I just wrote some code in swift! :metal:

---
+++ DatLog/Configuration.swift
@@ -0,0 +1,18 @@
+//
+//  Configuration.swift
+//  DatLog
+//
+//  Created by Yoni Kalangan on 17/08/2017.
+//  Copyright © 2017 Yoni Kalangan. All rights reserved.
+//
+
+import Foundation
+
+class Configuration {
+    private var printToLogout: Bool?
+    
+    class func setup() -> Configuration {
+        return Configuration()
+    }
+
+}
---
+++ DatLog/DatLog.swift
@@ -0,0 +1,21 @@
+//
+//  DatLog.swift
+//  DatLog
+//
+//  Created by Yoni Kalangan on 17/08/2017.
+//  Copyright © 2017 Yoni Kalangan. All rights reserved.
+//
+
+import Foundation
+
+
+class DatLog {
+
+    var config: Configuration;
+    
+    public init() {
+        config = Configuration.setup()
+    }
+    
+    
+}
