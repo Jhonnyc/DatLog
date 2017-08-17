b5cde1df16c7d75a6bf15f5a44d99770fb49b5c5
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-08-17 08:16:12 +0300

    Initial Commit

---
+++ DatLog/DatLog.h
@@ -0,0 +1,19 @@
+//
+//  DatLog.h
+//  DatLog
+//
+//  Created by Yoni Kalangan on 17/08/2017.
+//  Copyright © 2017 Yoni Kalangan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DatLog.
+FOUNDATION_EXPORT double DatLogVersionNumber;
+
+//! Project version string for DatLog.
+FOUNDATION_EXPORT const unsigned char DatLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DatLog/PublicHeader.h>
+
+
